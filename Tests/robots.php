02922b82d2a29046e89f52bb8510e2e1a7b5d0fd
Oleg <smaksss@mail.ua>
2015-03-24 20:43:07 +0300

    #11 модифицирован тест robots.

--- Tests/robots.php
+++ Tests/robots.php
@@ -2,27 +2,23 @@
 	namespace Tests;
 	class robots extends \Component\SiteTester\TestPrototype { // Класс тестирования файла robots.txt
 		protected function execute() { // выполнить тест
+			$search = array('aaaa', 'bbbb', 'cccc', 'dddd'); // Массив поиска
+			$sum = count($search); // Счётчик совпадений
+
 			if ( $handle = @fopen (SITE_DIR.'robots.txt','r') ) {
 				$newsearch = 0;
 				while (!feof($handle)) {
 					$line = strtolower(trim(fgets($handle)));
-					if (strpos($line, "user-agent:") === 0) $newsearch = 1;
-					switch ($newsearch) {
-						case 1:
-							if ($line === "user-agent: *") $newsearch = 2;
-							break;
-
-						case 2:
-							if ($line === "disallow: /") {
-						  		$this->setStatusOk('Тест пройден успешно');
-						  		$newsearch = 3;
-							}
-							break 2;
+					$i = array_search($line, $search);
+					if ($i !== false) {
+						unset($search[$i]);
+						$sum--;
 					}
 				}
 				fclose($handle);
-				if (!$newsearch) $this->addMessageWarning('Не найдена директива User-agent');
-				if ($newsearch != 3) $this->fail('Тест не выполнен');
+				if (!$sum) {
+					$this->setStatusOk();
+				} else $this->fail('Тест не выполнен');
 			} else {
 				$this->addMessageError('Не удаётся открыть файл robots.txt');
 				$this->setStatusError('Ошибка выполнения');
